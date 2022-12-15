567a0e286cbc14b9ee895bb965396099c9a28b9d
Tobias Aagaard <Tobias909@live.dk>
2022-12-15 20:42:11 +0100

    init router

---
+++ Routes/init.router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { sequelize } from "../Config/sequelize.config.js";
+const InitRouter = express.Router();
+
+InitRouter.get("/init", (req, res) => {
+  try {
+    sequelize.sync();
+    res.sendStatus(200);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+export default InitRouter;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import InitRouter from "./Routes/init.router";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -8,6 +9,9 @@ const app = express();
 app.get("/", (res, req) => {
   res.send("Denne server");
 });
+
+//router bundels
+app.use(InitRouter);
 
 app.listen(4000, () => {
   console.log("Server is running on localhost ");
