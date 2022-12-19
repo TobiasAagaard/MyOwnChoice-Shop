72ebed3b7b4c261de4171e55913cc7e052811a08
Tobias Aagaard <Tobias909@live.dk>
2022-12-19 15:09:47 +0100

    fix

--- Config/sequelize.config.js
+++ Config/sequelize.config.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import InitRouter from "./Routes/init.router";
+import InitRouter from "./Routes/init.router.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
